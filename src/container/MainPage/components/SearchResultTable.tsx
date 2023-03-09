b24bd9b42bdc18b37a16d53aa25b3658d7306deb
Jack <jack.hsieh@kangdainfo.com>
2023-03-09 16:20:43 +0800

    feat: add MainPage install react-window

--- src/container/MainPage/components/SearchResultTable.tsx
+++ src/container/MainPage/components/SearchResultTable.tsx
@@ -1,15 +1,57 @@
 import { storesType } from '@container/reducers';
-import React from 'react';
+import TableBody from '@modules/TableBody';
 import { useSelector } from 'react-redux';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { FixedSizeList as List } from 'react-window';
+import { tableTitle } from '../model/data';
+import './css/searchResultTable.scss';
+
+interface Props {
+    index?: any;
+    style?: any;
+    data?: any;
+}
+
+function Row({ index, style, data }: Props) {
+    return (
+        <div key={index} style={style} className="searchResultTable__tableRow">
+            <div className="searchResultTable__tableRow_item">{data[index].trainNumber}</div>
+            <div className="searchResultTable__tableRow_item">{data[index].departureTime}</div>
+            <div className="searchResultTable__tableRow_item">{data[index].arrivalTime}</div>
+            <div className="searchResultTable__tableRow_item">{data[index].travelTime}</div>
+        </div>
+    );
+}
 
 function SearchResultTable() {
     const { searchResult } = useSelector((state: storesType) => ({ ...state.mainPage }));
 
-    React.useEffect(() => {
-        console.log(searchResult);
-    }, [searchResult]);
+    return (
+        <div className="searchResultTable__container">
+            <div className="searchResultTable__tableHead">
+                {tableTitle.map((item) => (
+                    <div key={item.id} className="searchResultTable__tableHead_title">
+                        {item.title}
+                    </div>
+                ))}
+            </div>
 
-    return <div>index</div>;
+            <TableBody>
+                <AutoSizer>
+                    {({ height, width }) => (
+                        <List
+                            itemData={searchResult}
+                            itemCount={searchResult.length}
+                            height={height}
+                            itemSize={searchResult.length}
+                            width={width}>
+                            {Row}
+                        </List>
+                    )}
+                </AutoSizer>
+            </TableBody>
+        </div>
+    );
 }
 
 export default SearchResultTable;
--- src/container/MainPage/model/data.ts
+++ src/container/MainPage/model/data.ts
@@ -27,3 +27,10 @@ export const dateRestrict = {
     min: new Date().toISOString().slice(0, 10),
     max: getMaxDay()
 };
+
+export const tableTitle = [
+    { id: 'title01', title: '車次' },
+    { id: 'title02', title: '出發' },
+    { id: 'title03', title: '到達' },
+    { id: 'title04', title: '總時間' }
+];
