6abf34b19192a8aa89ff8a88818ce9e2c874506a
Jack <jack.hsieh@kangdainfo.com>
2023-03-14 13:57:36 +0800

    feat: add MainPage ParentModal style and display data

--- src/container/MainPage/components/ParentModal.tsx
+++ src/container/MainPage/components/ParentModal.tsx
@@ -3,15 +3,36 @@ import Modal from '@modules/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { SET_MODAL_TYPE, SET_TRAIN_ITEM } from '../model';
 import { searchResultData as initSearchResultData } from '../model/data';
+import { Options } from '../model/types';
 import './css/parentModal.scss';
 
 function ParentModal() {
     const dispatch = useDispatch();
     const {
         modalType,
-        parentModalData: { startStation, endStation, trainStop }
-        // trainItem: { trainNumber, departureTime, arrivalTime, travelTime }
+        parentModalData: { trainStop },
+        searchFormField: { startStationId, endStationId },
+        trainItem: { trainNumber, departureTime, arrivalTime, travelTime },
+        options
     } = useSelector((state: storesType) => ({ ...state.mainPage }));
+
+    const handleInRange = (stationId: string) => {
+        const start = Number(startStationId);
+        const end = Number(endStationId);
+        const station = Number(stationId);
+        return station < end && station > start;
+    };
+
+    const handleRangeStyleType = (stationId: string, type?: string) => {
+        if (stationId === startStationId) return `active ${type ? '' : 'first'}`;
+        if (stationId === endStationId) return `active  ${type ? '' : 'last'}`;
+        if (handleInRange(stationId)) return 'active';
+    };
+
+    const handleGetStationName = (stationId: string) => {
+        const stationItem: Array<Options> = options.filter((option) => option.id === stationId);
+        return stationItem[0]?.name;
+    };
 
     const handleCloseModal = () => {
         dispatch(SET_MODAL_TYPE(false));
@@ -21,26 +42,34 @@ function ParentModal() {
     return (
         <Modal isOpen={modalType} handleCancel={handleCloseModal}>
             <div className="parentModal__title">
-                <div className="parentModal__title_from">{startStation}</div>
+                <div className="parentModal__title_from">{handleGetStationName(startStationId)}</div>
                 <div className="parentModal__title_arrow">
                     <span>去程</span>
                 </div>
-                <div className="parentModal__title_to">{endStation}</div>
+                <div className="parentModal__title_to">{handleGetStationName(endStationId)}</div>
             </div>
 
-            {/* <div className="parentModal__timeField">
-                <div className="parentModal__timeField_departureTime">{departureTime}</div>
-                <div className="parentModal__timeField_arrow">{travelTime}</div>
-                <div className="parentModal__timeField_arrivalTime">{arrivalTime}</div>
-                <div className="parentModal__timeField_trainNumber">{trainNumber}</div>
-            </div> */}
+            <div className="parentModal__timeField">
+                <div className="parentModal__timeField_row title">
+                    <div className="parentModal__timeField_item">出發</div>
+                    <div className="parentModal__timeField_item">抵達</div>
+                    <div className="parentModal__timeField_item">行車</div>
+                    <div className="parentModal__timeField_item">車次</div>
+                </div>
+                <div className="parentModal__timeField_row">
+                    <div className="parentModal__timeField_item departureTime">{departureTime}</div>
+                    <div className="parentModal__timeField_item arrivalTime">{arrivalTime}</div>
+                    <div className="parentModal__timeField_item arrow">{travelTime}</div>
+                    <div className="parentModal__timeField_item trainNumber">{trainNumber}</div>
+                </div>
+            </div>
 
             <div className="parentModal__field">
                 <ul className="parentModal__field_container">
                     {trainStop?.map((item) => (
-                        <li className="parentModal__field_item">
+                        <li key={item.id} className={`parentModal__field_item ${handleRangeStyleType(item.id)}`}>
                             <div>{item.stationName}</div>
-                            <span className="dot" />
+                            <span className={`dot ${handleRangeStyleType(item.id, 'dot')}`} />
                             <p>{item.arrivalTime}</p>
                         </li>
                     ))}
