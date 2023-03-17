5d1485f84821459024fbf07cd19f515b38077c23
Jack <jack.hsieh@kangdainfo.com>
2023-03-17 14:19:29 +0800

    feat: add Project assets path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
             '@utils': path.resolve(__dirname, 'src/utils'),
             '@configs': path.resolve(__dirname, 'src/configs'),
             '@service': path.resolve(__dirname, 'src/service'),
